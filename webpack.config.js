05394e6b4d4accecf58f61562c5d6581b6772971
Mike <mhwilkes@gmail.com>
2019-03-15 15:58:26 -0400

    Base Build
    Signed-off-by: Mike <mhwilkes@gmail.com>

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const webpack = require("webpack");
+const path = require("path");
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader"
+        ],
+        exclude: /\.module\.css$/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              mimetype: "image/png"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: "file-loader"
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]"
+        ],
+        include: /\.module\.css$/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      ".js",
+      ".jsx"
+    ]
+  },
+  devServer: {
+    contentBase: "./dist"
+  }
+};
+
+module.exports = config;

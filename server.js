4148560f142a2fe5d689159478c59bd7e64ce581
Mike <mhwilkes@gmail.com>
2019-05-10 10:07:18 -0400

    functional routing

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(__dirname + '/'));
 
-require('./src/index')(app);
+require('src/index')(app);
 
 app.get('*', (req,res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));

39dd5e3e2dc769e60c3788ddebeb9c6303dfbea5
Mike <mhwilkes@gmail.com>
2019-05-10 10:20:42 -0400

    fix server.js

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get('/ping', function (req, res) {
   return res.send('pong');
 });
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 app.listen(port);

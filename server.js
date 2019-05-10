191215bf15630352fc6d70949fa78645b6760d5c
Mike <mhwilkes@gmail.com>
2019-05-10 10:17:42 -0400

    functional routing

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname + '/'));
-
-require('src/index')(app);
-
-app.get('*', (req,res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('/ping', function (req, res) {
+  return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port);
-console.log('server started');

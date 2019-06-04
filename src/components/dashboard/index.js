e7931573184122be17e6c7a29a319381159515ba
Michael Wilkes <mhwilkes@gmail.com>
2019-06-04 10:12:05 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/assets/styles/variables.scss
    #	src/components/home/home.scss
    #	src/components/login/login.scss

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,7 +1,13 @@
 import React from "react";
-import { Container, ListGroup, ListGroupItem } from "reactstrap";
+import { Container, ListGroup, ListGroupItem, Row, Col } from "reactstrap";
 import { Link } from "react-router-dom";
 import ".//dashboard.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithubSquare } from "@fortawesome/free-brands-svg-icons";
+import { faFolder } from "@fortawesome/free-solid-svg-icons/faFolder";
+import { faDog } from "@fortawesome/free-solid-svg-icons/faDog";
+import { faTrash } from "@fortawesome/free-solid-svg-icons/faTrash";
+import { faFighterJet } from "@fortawesome/free-solid-svg-icons/faFighterJet";
 
 export default class Dashboard extends React.Component {
   constructor(props) {
@@ -14,18 +20,27 @@ export default class Dashboard extends React.Component {
       <React.Fragment>
         <aside className="dashboard-sidebar d-inline-block">
           <ListGroup flush>
-            <ListGroupItem tag={Link} href="#">Cras justo odio</ListGroupItem>
-            <ListGroupItem tag={Link} href="#">Dapibus ac facilisis in</ListGroupItem>
-            <ListGroupItem tag={Link} href="#">Morbi leo risus</ListGroupItem>
-            <ListGroupItem tag={Link} href="#">Porta ac consectetur ac</ListGroupItem>
-            <ListGroupItem tag={Link} href="#">Vestibulum at eros</ListGroupItem>
+            <ListGroupItem tag={Link} href="#"><FontAwesomeIcon icon={faGithubSquare}/> Integration</ListGroupItem>
+            <ListGroupItem tag={Link} href="#"><FontAwesomeIcon icon={faFolder}/> Projects</ListGroupItem>
+            <ListGroupItem tag={Link} href="#"><FontAwesomeIcon icon={faDog}/> Dog</ListGroupItem>
+            <ListGroupItem tag={Link} href="#"><FontAwesomeIcon icon={faTrash}/> Trash</ListGroupItem>
+            <ListGroupItem tag={Link} href="#"><FontAwesomeIcon icon={faFighterJet}/> Top Gun</ListGroupItem>
           </ListGroup>
         </aside>
         <Container fluid className="dashboard d-inline-block">
-          Dashboard
+          <Row>
+            <Col lg={4} md={6} sm={12} xs={12} className="text-center">
+              <h2>Dog</h2>
+            </Col>
+            <Col lg={4} md={6} sm={12} xs={12} className="text-center">
+              <h2>Top Gun</h2>
+            </Col>
+            <Col lg={4} md={6} sm={12} xs={12} className="text-center">
+              <h2>Trash</h2>
+            </Col>
+          </Row>
         </Container>
       </React.Fragment>
-
     );
   }
 }

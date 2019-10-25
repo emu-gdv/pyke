0b955d79e65d0d61b729473a94103a86642999df
Mike <mhwilkes@gmail.com>
2019-10-25 18:39:59 -0400

    fixing mailgun sending

--- src/components/contact-us/index.js
+++ src/components/contact-us/index.js
@@ -1,10 +1,31 @@
 import React from "react";
-import { Button, Col, Container, Form, FormFeedback, FormGroup, Input, Label, Row } from "reactstrap";
+import { Button, Col, Container, Form, FormFeedback, FormGroup, Input, Label, Row, Alert } from "reactstrap";
 import ".//contact.scss";
 
 export default class Contact extends React.Component {
 
+  emailNotification = message => {
+    switch (message) {
+    case 200:
+      return <Alert color="warning">
+          Thank you for your submission, we will get back with you soon!
+      </Alert>;
+    case 400:
+      return <Alert color="danger">
+          Your submission was not sent, please contact us at sgw_dev@emich.edu.
+      </Alert>;
+    case "":
+      return <Alert color="light">
+      </Alert>;
+    default:
+      return null;
+    }
+  };
+
   render() {
+
+    const { handleSubmit, message } = this.props;
+
     return (
       <Container>
         <Row>
@@ -15,7 +36,8 @@ export default class Contact extends React.Component {
             <p>Sub Heading Text</p>
           </Col>
           <Col sm={8}>
-            <Form method="post">
+            <Form onSubmit={handleSubmit}>
+              {this.emailNotification(message)}
               <FormGroup>
                 <Label className="mb-2 mr-2" for="name">Name</Label>
                 <Input className="mb-2" type="text" name="name" id="name" placeholder="John Smith"/>

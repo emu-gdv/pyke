c4b8d791c2463bb632ac360c8edf5e2e292ddc97
Mike <mhwilkes@gmail.com>
2019-10-25 09:24:57 -0400

    testing mailgun addition

---
+++ config/mail/mailgun.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const Mailgun = require("mailgun-js");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const mg = new Mailgun(process.env.MAILGUN_API_KEY);
+
+// Setup view engine
+app.set("view engine", "pug");
+
+// Parse form data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => res.render("index"));
+
+app.post("/contact-us", (req, res, next) => {
+  const servername = "";
+  const options = {};
+
+  mg.sendText(
+    // From
+    "info@sgwdev.org",
+    // To
+    req.body.email,
+    // Subject
+    "Test Email",
+    // Body
+    "Mailgun on App Engine with Node.js",
+    servername,
+    options,
+    err => {
+      if (err) {
+        next(err);
+        return;
+      }
+      // Render the index route on success
+      res.render("index", {
+        sent: true
+      });
+    }
+  );
+});
--- server.js
+++ server.js
@@ -4,12 +4,13 @@ require("dotenv").config();
 // require("./config/passport-strategies/github-oauth");
 require("./config/routes/routes");
 // require("./config/models/user");
+require("./config/mail/mailgun");
 
 const express = require("express");
 const path = require("path");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const session = require("express-session");
+// const bodyParser = require("body-parser");
+// const cookieParser = require("cookie-parser");
+// const session = require("express-session");
 const port = process.env.PORT || 3000;
 const app = express();
 
@@ -19,30 +20,30 @@ app.use(history());
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "dist")));
 
-const mongoose = require("mongoose");
-const passport = require("passport");
+// const mongoose = require("mongoose");
+// const passport = require("passport");
 
 // mongoose.connect(process.env["MONGODB_URI"], { useNewUrlParser: true });
 // const db = mongoose.connection;
 
-// BodyParser Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-// Express Session
-app.use(session({
-  secret: "secret",
-  saveUninitialized: true,
-  resave: true
-}));
-
-// Passport init
-app.use(passport.initialize());
-app.use(passport.session());
-/*
-Passport JS Section
- */
+// // BodyParser Middleware
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+//
+// // Express Session
+// app.use(session({
+//   secret: "secret",
+//   saveUninitialized: true,
+//   resave: true
+// }));
+//
+// // Passport init
+// app.use(passport.initialize());
+// app.use(passport.session());
+// /*
+// Passport JS Section
+//  */
 
 
 app.listen(port, () => console.log("Express App Listening on " + port.toString()));

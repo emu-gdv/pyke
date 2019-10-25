f8aaf5c30e17d478d325a598c123c11de3969444
Mike <mhwilkes@gmail.com>
2019-10-25 10:42:01 -0400

    fixing mailgun sending

---
+++ config/mail/email-handle.js
@@ -0,0 +1,41 @@
+const mailgun = require("mailgun-js");
+const DOMAIN = 'dev.sgwdev.org';
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Parse form data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: DOMAIN});
+
+app.post('/email-handle.js', (req, res, next) => {
+  const servername = '';
+  const options = {};
+
+  mg.sendText(
+    // From
+    'info@dev.sgwdev.org',
+    // To
+    req.body.email,
+    // Subject
+    'Contact Form Submission',
+    // Body
+    'Testing Mailgun',
+    servername,
+    options,
+    (err) => {
+      if (err) {
+        next(err);
+        return;
+      }
+      // Render the index route on success
+      res.render('/thank-you', {
+        sent: true
+      });
+    }
+  );
+});
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 // require("./config/passport-strategies/github-oauth");
 require("./config/routes/routes");
 // require("./config/models/user");
-require("./config/mail/email_handle");
+require("./config/mail/email-handle");
 
 const express = require("express");
 const path = require("path");

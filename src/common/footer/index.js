4e2fc9f5769d9a282fe42fae9b9a9170e5a7ad11
Mike <mhwilkes@gmail.com>
2019-10-25 21:46:42 -0400

    Home page additions

--- src/common/footer/index.js
+++ src/common/footer/index.js
@@ -64,7 +64,7 @@ export default class Footer extends React.Component {
                   </a>
                 </li>
                 <li>
-                  <a href="https://www.instagram.com/sgwdev/">
+                  <a target="_blank" href="https://www.instagram.com/sgwdev/">
                     Instagram
                   </a>
                 </li>
